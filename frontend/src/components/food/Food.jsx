f117cd64617a8515d9d62e4569ac0148015d6444
Kiet Phan <22520688@gm.uit.edu.vn>
2024-09-28 20:19:58 +0700

    store-sizeFixed

--- frontend/src/components/food/Food.jsx
+++ frontend/src/components/food/Food.jsx
@@ -61,7 +61,7 @@ const Food = () => {
             Best Food
         </h1>
         
-          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 transition-all duration-300 ease-in-out">
             {FoodData.map((item, index) => (
               <FoodCard
                 key={index}
--- frontend/src/pages/Store/StorePage.jsx
+++ frontend/src/pages/Store/StorePage.jsx
@@ -7,10 +7,10 @@ import React, { useState } from 'react'
 function StorePage() {
     const [sliderValue, setSliderValue] = useState([20,100]);
   return (
-    <section className='rounded-xl mx-32 h-full mt-20'>
+    <section className='rounded-xl mx-5 h-full mt-20 min-w-md'>
         <div className="shopInfo">
             <div className="relative bg-center w-full bg-cover h-[500px] bg-[url('https://t3.ftcdn.net/jpg/02/05/03/96/360_F_205039646_r2ddysNHjVE5HFJUQnMS6UcgqYgjQ0H8.jpg')] rounded-xl flex lg:pl-24 md:pl-8 sm:pl-0">
-                <div className="w-fit flex flex-col items-center py-40">
+                <div className="w-fit flex flex-col items-center py-40 sm:mx-5">
                     {/* Shop Info */}
                     <div className="userInfo flex flex-col md:flex-row px-8 items-center bg-white rounded-xl">
                     <div className="profilePic bg-[url('https://thietkelogo.mondial.vn/wp-content/uploads/2024/03/burger-king-vector.jpg')] lg:w-[150px] lg:h-[150px] w-[100px] h-[100px] bg-cover rounded-full"></div>
@@ -32,12 +32,13 @@ function StorePage() {
                 </div>
             </div>
         </div>
-        <div className="shopItemSection mt-10 bg-theme-color rounded-lg h-full">
+        <div className="shopItemSection mt-10 bg-theme-color rounded-lg h-full ">
             <div className="shopItemList flex justify-between gap-5">
-                <div className="leftUtils w-1/4 flex flex-col gap-3 h-full ">
-                    <div className="searchByType  text-center w-full bg-white mt-5 ml-1 items-center rounded-md">
+                <div className="leftUtils w-1/4 flex flex-col gap-3 h-full ml-5">
+                    {/* Search By Type Section */}
+                    <div className="searchByType text-center w-full bg-white mt-5 items-center rounded-md">
                         <h1 className="mt-2 border-b-red-700 border-b-2 w-full text-center">Search</h1>
-                        <div className="search-grid grid grid-cols-3 grid-rows-2 gap-4 p-4"> 
+                        <div className="search-grid grid grid-cols-2 md:grid-cols-3 grid-rows-2 gap-4 p-4">
                             <Button className="bg-light-them-color hover:bg-orange-300 text-black hover:scale-105">Type</Button>
                             <Button className="bg-light-them-color hover:bg-orange-300 text-black hover:scale-105">Pizza</Button>
                             <Button className="bg-light-them-color hover:bg-orange-300 text-black hover:scale-105">Type</Button>
@@ -46,23 +47,22 @@ function StorePage() {
                             <Button className="bg-light-them-color hover:bg-orange-300 text-black hover:scale-105">Type</Button>
                         </div>
                     </div>
-                    <div className="filterByPrice text-center w-full bg-white mt-5 ml-1 items-center rounded-md h-[100px]">
-                            <h1 className="mt-2 border-b-red-700 border-b-2 w-full text-center">Filter by price</h1>
-                            <div className="priceSlider">
-                            <Slider
-                                    className="progress py-2 px-2 text-black"
-                                    defaultValue={[33]} max={100} step={1} onValueChange={setSliderValue}
-                                />
-                            </div>
-                            <div className="flex justify-between mt-2 px-5">
+
+                    {/* Filter by Price Section */}
+                    <div className="filterByPrice text-center w-full bg-white mt-5 items-center rounded-md pb-5">
+                        <h1 className="mt-2 border-b-red-700 border-b-2 w-full text-center">Filter by price</h1>
+                        <div className="priceSlider">
+                            <Slider className="progress py-2 px-2 text-black" defaultValue={[33]} max={100} step={1} onValueChange={setSliderValue} />
+                        </div>
+                        <div className="flex justify-between mt-2 px-5">
                             <span>From: $0</span>
                             <span>To: ${sliderValue[0]}</span>
-                            </div>
+                        </div>
                     </div>
-                    <div className="trending text-center w-full bg-white mt-5 ml-1 items-center rounded-md h-full">
+                    <div className="trending text-center w-full bg-white mt-5 items-center rounded-md h-full">
                         <h1 className="mt-2 border-b-red-700 border-b-2 w-full text-center">Popular dishes</h1>
-                        <div className="item-container flex flex-col h-[300px] mt-3 gap-1">
-                            <div className="item flex border border-orange-400 mx-3 rounded-xl gap-3" >
+                        <div className="item-container flex flex-col  mt-3 gap-1 pb-5">
+                            <div className="item flex border border-orange-400 mx-3 rounded-xl gap-3 p-2 lg:flex-row lg:justify-between lg:px-5 md:flex-col md:items-center">
                                 <div className="pic bg-[url('https://upload.wikimedia.org/wikipedia/commons/4/4d/Big_Mac.png')] w-[100px] h-[90px] bg-cover mx-2">
                                 </div>
                                 <div className="content flex flex-col justify-center gap-2">
@@ -73,7 +73,7 @@ function StorePage() {
                                     <Button>Add to cart</Button>
                                 </div>
                             </div>
-                            <div className="item flex border border-orange-400 mx-3 rounded-xl gap-3" >
+                            <div className="item flex border border-orange-400 mx-3 rounded-xl gap-3 p-2 lg:flex-row lg:justify-between lg:px-5 md:flex-col md:items-center">
                                 <div className="pic bg-[url('https://upload.wikimedia.org/wikipedia/commons/4/4d/Big_Mac.png')] w-[100px] h-[90px] bg-cover mx-2">
                                 </div>
                                 <div className="content flex flex-col justify-center gap-2">
@@ -84,7 +84,7 @@ function StorePage() {
                                     <Button>Add to cart</Button>
                                 </div>
                             </div>
-                            <div className="item flex  border border-orange-400 mx-3 rounded-xl gap-3" >
+                            <div className="item flex border border-orange-400 mx-3 rounded-xl gap-3 p-2 lg:flex-row lg:justify-between lg:px-5 md:flex-col md:items-center">
                                 <div className="pic bg-[url('https://upload.wikimedia.org/wikipedia/commons/4/4d/Big_Mac.png')] w-[100px] h-[90px] bg-cover mx-2">
                                 </div>
                                 <div className="content flex flex-col justify-center gap-2">
