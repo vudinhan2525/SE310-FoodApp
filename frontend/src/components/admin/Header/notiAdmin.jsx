18d28e75d53b57a7e6a20ad54338602c1b364a7a
duc190604 <haminhduchaminhduc@gmail.com>
2024-12-19 20:27:29 +0700

    update noti

--- frontend/src/components/admin/Header/notiAdmin.jsx
+++ frontend/src/components/admin/Header/notiAdmin.jsx
@@ -1,15 +1,15 @@
 import notiApi from "@/apis/notiApi";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useState, useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
-const NotiCard = ({ userId }) => {
+const NotiAdmin = () => {
   const [notifications, setNotifications] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const fetchNotifications = async () => {
     try {
-      const response = await notiApi.getNotiByUser(1, 20, userId);
+      const response = await notiApi.getAllNoti();
       setNotifications(response.data);
     } catch (error) {
       console.log(error);
@@ -19,16 +19,8 @@ const NotiCard = ({ userId }) => {
   };
   useEffect(() => {
     fetchNotifications();
-  }, [userId]);
+  }, []);
 
-  // const handleDeleteNotification = async (notificationId) => {
-  //   try {
-  //     await notiApi.deleteNotification(notificationId);
-  //     fetchNotifications();
-  //   } catch (error) {
-  //     console.log("Error deleting notification:", error);
-  //   }
-  // };
   useEffect(() => {
     console.log("Updated notifications:", notifications);
   }, [notifications]);
@@ -38,8 +30,8 @@ const NotiCard = ({ userId }) => {
   }
 
   return (
-    <div className="absolute right-[-40px] mt-2 bg-white shadow-lg rounded-md w-[350px] p-4 z-50">
-      <h4 className="font-semibold text-lg mb-2">Notifications</h4>
+    <div className="absolute right-[-40px] mt-2 bg-white shadow-lg rounded-md w-[350px] py-4 px-2 z-50 border-[1.5px] border-b-0 border-gray-200">
+      <h4 className="font-semibold text-lg mb-2 ml-2">Notifications</h4>
       <ScrollArea className="max-h-[300px] overflow-y-auto px-3">
         {notifications?.length > 0 ? (
           notifications.map((notification, index) => (
@@ -59,4 +51,4 @@ const NotiCard = ({ userId }) => {
   );
 };
 
-export default NotiCard;
+export default NotiAdmin;
